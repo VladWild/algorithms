a2107d2daa2e81e92078b4045bbaf18f4172a132
Vlad Wild <vladwild220@gmail.com>
2022-06-04 11:48:46 +0300

    add deque

--- src/com/algorithms/deque/MainDequeStarter.java
+++ src/com/algorithms/deque/MainDequeStarter.java
@@ -1,8 +1,23 @@
 package com.algorithms.deque;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class MainDequeStarter {
 
     public static void main(String[] args) {
+        Deque<String> deque = new ArrayDeque<>();
+        //Deque<String> deque = new LinkedList<>();
+        deque.add("Игорь");
+        deque.add("Саша");
+        deque.add("Катя");
+        deque.add("Миша");
 
+        Iterator<String> iterator = deque.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
